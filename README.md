13a45dfc4306a7a6f09749d0d571c757267fe198
justin <junejwind@163.com>
2020-02-17 17:53:11 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+
 # hong
 java
+
---
+++ src/test/java/com/wjj/hong/HongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjj.hong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
